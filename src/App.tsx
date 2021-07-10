3f7c674d79cf28e9130c1acc95abf7495420ab19
dukwar <d.clopow@yandex.ru>
2021-07-10 18:39:34 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {start} from "./snake/app";
 
 function App() {
+
+    useEffect(() => {
+        start()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas id="canvas" width="1000" height="700">
+
+    </canvas>
   );
 }
 
---
+++ src/snake/app.ts
@@ -0,0 +1,26 @@
+export function start() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
